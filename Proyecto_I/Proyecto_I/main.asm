f3b41ac6ff3a471db75c8cdb9dc35fcbf82aaaef
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2025-03-09 00:37:11 -0600

    Multiplexeo de leds y cambios de estado funcional
    El problema es con el

--- Proyecto_I/Proyecto_I/main.asm
+++ Proyecto_I/Proyecto_I/main.asm
@@ -16,6 +16,7 @@
 .def	SET_PB=R17							//PUERTO C
 .def	DISPLAY=R18							//PUERTO D
 .def	MULTIPLEX_DISP=R19					//PUERTO B
+.def	FLAG_STATE=R20						//Modos	
 
 
 .org 0x0000
@@ -38,7 +39,7 @@ SETUP:
 	//Configuracion de puerto C
 	LDI		R16, 0x10						//PINC0/4 entrada y PC5 salida
 	OUT		DDRC, R16
-	LDI		R16, 0x0F						//PINC0/4 pullup activados y PC5 conduce 0 logico
+	LDI		R16, 0b00011111						//PINC0/4 pullup activados y PC5 conduce 0 logico
 	OUT		PORTC, R16
 
 	//Configuracion de puerto B
@@ -55,9 +56,9 @@ SETUP:
 
 	//Habilitar interrupciones en el puerto c
 	LDI		R16, (PCIE1<<1)					//Setear PCIE1 en PCICR
-	OUT		PCICR, R16						
+	STS		PCICR, R16						
 	LDI		R16, 0x1F						//Activar las interrupciones solo en los pines de botones
-	OUT		PCMSK1, R16
+	STS		PCMSK1, R16
 
 	//Deshabilitar comunicacion serial
 	LDI		R16, 0x00
@@ -67,7 +68,8 @@ SETUP:
 	LDI		SET_PB, 0x00
 	LDI		MULTIPLEX_DISP, 0x00
 	LDI		DISPLAY, 0x00
-	
+	LDI		FLAG_STATE, 0x00				//Por default inicia en el modo hora.
+
 	//Usar el puntero Z como salida de display
 	LDI		ZH, HIGH(TABLA<<1)				//Carga la parte alta de la direcci?n de tabla en el registro ZH
 	LDI		ZL, LOW(TABLA<<1)				//Carga la parte baja de la direcci?n de la tabla en el registro ZL
@@ -77,14 +79,100 @@ SETUP:
 	SEI										//Habilitar interrupciones globales
 
 MAIN:
-	RJMP MAIN
+	//Utilizo flag state para saber en que modo estoy
+	CPI		FLAG_STATE, 0x00
+	BREQ	HORA
+	CPI		FLAG_STATE, 0x01
+	BREQ	FECHA
+	CPI		FLAG_STATE, 0x02
+	BREQ	CONFI_HORA
+	CPI		FLAG_STATE, 0x03
+	BREQ	CONFI_FECHA
+	CPI		FLAG_STATE, 0x04
+	BREQ	CONFI_ALARMA
+	CPI		FLAG_STATE, 0x05
+	BREQ	OFF_ALARMA
+	RJMP	MAIN
 
+//*************Modos**************
+HORA:
+	LPM		DISPLAY, Z
+	OUT		PORTD, DISPLAY
+	LDI		R16,  0x0F						//Encender los display.
+	OUT		PORTB, R16
+	//Apagar todas las leds de estado	
+	SBI		PORTB, 4						
+	RJMP	MAIN
 
+FECHA:
+	LPM		DISPLAY, Z
+	OUT		PORTD, DISPLAY
+	LDI		R16,  0x01						//Encender el primero display.
+	OUT		PORTB, R16
+	//Apagar todas las leds de estado	
+	SBI		PORTB, 4
+	SBI		PORTC, 5						
+	RJMP	MAIN
+		
+CONFI_HORA:
+	LPM		DISPLAY, Z
+	OUT		PORTD, DISPLAY
+	LDI		R16,  0x02						//Encender el primero display.
+	OUT		PORTB, R16
+	//encender la led de la hora
+	LDI		DISPLAY, 0x00
+	OUT		PORTD, DISPLAY
+	SBI		PORTB, 4
+	CBI		PORTC, 5
+	RJMP	MAIN
+
+CONFI_FECHA:
+	LPM		DISPLAY, Z
+	OUT		PORTD, DISPLAY
+	LDI		R16,  0x03						//Encender el primero display.
+	OUT		PORTB, R16
+	LDI		MULTIPLEX_DISP, 0x07
+	OUT		PORTB, MULTIPLEX_DISP
+	//Encender la led de la fecha
+	CBI		PORTB, 4
+	CBI		PORTC, 5					
+	RJMP	MAIN
+
+CONFI_ALARMA:
+	LPM		DISPLAY, Z
+	OUT		PORTD, DISPLAY
+	LDI		R16,  0x04						//Encender el primero display.
+	OUT		PORTB, R16
+	//Encender la led de la alarma
+	//LDI		R16, 0b00011111	
+	//OUT		PORTC, R16
+	SBI		PORTC, 5
+	RJMP	MAIN
+
+OFF_ALARMA:
+	LPM		DISPLAY, Z
+	OUT		PORTD, DISPLAY
+	LDI		R16,  0x05						//Encender el primero display.
+	OUT		PORTB, R16
+	//Apagar todas las leds de estado	
+	SBI		PORTB, 4
+	SBI		PORTC, 5						
+	RJMP	MAIN
+//*************Modos**************
 
 
 ISR_PCINT1:
+	IN		SET_PB, PINC					//Leer el puerto C
+	SBRS	SET_PB, 4						//Verificar la bandera de cambio de modo
+	INC		FLAG_STATE						//Incrementar para cambiar de modo				
+	CPI		FLAG_STATE, 0x06				//Regresar al modo de hora.
+	BRNE	RETORNO
+	ADIW	Z, 1
+	LD		DISPLAY, Z
+	LDI		FLAG_STATE, 0x00				//Resetear el estado
+RETORNO:
 	RETI
 
 // Tabla de conversi?n hexadecimal a 7 segmentos
 TABLA:
-    .DB 0x77, 0x50, 0x3B, 0x7A, 0x5C, 0x6E, 0x6F, 0x70, 0x7F, 0x7E, 0x7D, 0x4F, 0x27, 0x5B, 0x2F, 0x2D	
+    .DB 0xF3, 0x81, 0xEA, 0xE9, 0x99, 0x79, 0x7B, 0XC9, 0xFB, 0xF9
